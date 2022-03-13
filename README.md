db49dbc5069f00c9e77029f43ef64164048c127d
Zeeshan Butt <zb0255209@gmail.com>
2022-03-13 21:06:47 +0500

    Comment dotenv library

---
+++ README.md
@@ -0,0 +1,28 @@
+# Personality Test Backend
+
+Backend of the Personality Test Website.
+
+---
+
+## Requirements
+
+For development, you will only need Node.js and a node global package, Yarn, installed in your environement.
+
+- [NodeJS](https://nodejs.org/en/)
+- [Yarn](https://yarnpkg.com/)
+
+---
+
+## Install
+
+    $ git clone https://github.com/mzeeshanbutt/personality-test-backend
+    $ cd personality-test-backend
+    $ yarn install
+
+## Running the project
+
+    $ yarn start
+
+## Simple build for production
+
+    $ yarn build
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Load the .env file in the root of the project
 // and initialize values
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 // Module imports
 import express from "express";
 import { JSONFile, Low } from "lowdb";
@@ -11,7 +11,7 @@ import cors from "cors";
 dotenv.config();
 
 // Load port from environment variables
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Initialize the express application
 const app = express();
@@ -97,6 +97,6 @@ app.patch("/question", async (req, res) => {
 });
 
 // Listen for the server
-app.listen(PORT, () => {
-  console.log(`Running application on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Running application on port ${3000}`);
 });
